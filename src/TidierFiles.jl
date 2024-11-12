685107fcfdcff594e4fe122203a6c5f6796a7865
Daniel Rizk <rizkytennis@gmail.com>
2024-11-11 19:50:45 -0500

    adds `col_types` and all args for csv.jl based readers

--- src/TidierFiles.jl
+++ src/TidierFiles.jl
@@ -31,6 +31,7 @@ $docstring_read_csv
 """
 function read_csv(files;
                   delim=',',
+                  decimal='.',
                   col_names=true,
                   skip=0,
                   n_max=Inf,
@@ -39,7 +40,9 @@ function read_csv(files;
                   missingstring="",
                   escape_double=true,
                   ntasks::Int = Threads.nthreads(),
-                  num_threads::Union{Int, Nothing}=nothing)
+                  num_threads::Union{Int, Nothing}=nothing, 
+                  col_types = nothing,
+                  kwargs...)  # Catch any other keyword arguments
 
     # Normalize input to always be a vector of files
     file_list = (typeof(files) <: AbstractString) ? [files] : files
@@ -53,21 +56,24 @@ function read_csv(files;
     # Calculate skipto and header
     skipto = skip + (col_names === true ? 1 : 0)
 
-    # Prepare CSV reading options
-    read_options = (
-        delim = delim,
-        header = col_names === true ? 1 : 0,
-        skipto = skipto + 1,
-        footerskip = 0,
-        select = col_select,
-        limit = limit,
-        comment = comment,
-        missingstring = missingstring,
-        escapechar = escape_double ? '"' : '\\',
-        quotechar = '"',
-        normalizenames = false,
-        ntasks = effective_ntasks > 1
+    read_options = Dict(
+        :delim => delim,
+        :decimal => decimal,
+        :header => col_names === true ? 1 : 0,
+        :skipto => skipto + 1,
+        :footerskip => 0,
+        :select => col_select,
+        :limit => limit,
+        :comment => comment,
+        :missingstring => missingstring,
+        :escapechar => escape_double ? '"' : '\\',
+        :quotechar => '"',
+        :ntasks => effective_ntasks > 1, 
+        :types => col_types
     )
+
+    # Merge additional keyword arguments into the read_options dictionary
+    merge!(read_options, kwargs)
 
     # Initialize an empty DataFrame
     final_df = DataFrame()
@@ -94,22 +100,24 @@ function read_csv(files;
     return final_df
 end
 
+
 """
 $docstring_read_delim
 """
 function read_delim(files;
-                    delim='\t',
-                    decimal='.',
-                    col_names=true,
-                    skip=0,
-                    n_max=Inf,
-                    groupmark=nothing,
-                    col_select=nothing,
-                    comment=nothing,
-                    missingstring="",
-                    escape_double=true,
-                    ntasks::Int = Threads.nthreads(),
-                    num_threads::Union{Int, Nothing}=nothing)
+                  delim=',',
+                  decimal='.',
+                  col_names=true,
+                  skip=0,
+                  n_max=Inf,
+                  col_select=nothing,
+                  comment=nothing,
+                  missingstring="",
+                  escape_double=true,
+                  ntasks::Int = Threads.nthreads(),
+                  num_threads::Union{Int, Nothing}=nothing, 
+                  col_types = nothing,
+                  kwargs...) 
 
     # Normalize input to always be a vector of files
     file_list = (typeof(files) <: AbstractString) ? [files] : files
@@ -124,22 +132,23 @@ function read_delim(files;
     skipto = skip + (col_names === true ? 1 : 0)
 
     # Prepare CSV reading options
-    read_options = (
-        delim = delim,
-        decimal = decimal,
-        header = col_names === true ? 1 : 0,
-        skipto = skipto + 1,
-        select = col_select,
-        groupmark = groupmark,
-        footerskip = 0,
-        limit = limit,
-        comment = comment,
-        missingstring = missingstring,
-        escapechar = escape_double ? '"' : '\\',
-        quotechar = '"',
-        normalizenames = false,
-        ntasks = effective_ntasks > 1
+    read_options = Dict(
+        :delim => delim,
+        :decimal => decimal,
+        :header => col_names === true ? 1 : 0,
+        :skipto => skipto + 1,
+        :select => col_select,
+        :footerskip => 0,
+        :limit => limit,
+        :comment => comment,
+        :missingstring => missingstring,
+        :escapechar => escape_double ? '"' : '\\',
+        :quotechar => '"',
+        :normalizenames => false,
+        :ntasks => effective_ntasks > 1,
+        :types => col_types
     )
+    merge!(read_options, kwargs)
 
     # Initialize an empty DataFrame
     final_df = DataFrame()
@@ -172,6 +181,7 @@ $docstring_read_tsv
 """
 function read_tsv(files;
                   delim='\t',
+                  decimal='.',
                   col_names=true,
                   skip=0,
                   n_max=Inf,
@@ -180,7 +190,10 @@ function read_tsv(files;
                   missingstring="",
                   escape_double=true,
                   ntasks::Int = Threads.nthreads(),
-                  num_threads::Union{Int, Nothing}=nothing)
+                  num_threads::Union{Int, Nothing}=nothing,
+                  col_types = nothing,
+                  groupmark=nothing,
+                  kwargs...)
 
     # Normalize input to always be a vector of files
     file_list = (typeof(files) <: AbstractString) ? [files] : files
@@ -194,21 +207,26 @@ function read_tsv(files;
     # Calculate skipto and header
     skipto = skip + (col_names === true ? 1 : 0)
 
-    # Prepare CSV reading options
-    read_options = (
-        delim = delim,
-        header = col_names === true ? 1 : 0,
-        skipto = skipto + 1,
-        footerskip = 0,
-        limit = limit,
-        select = col_select,
-        comment = comment,
-        missingstring = missingstring,
-        escapechar = escape_double ? '"' : '\\',
-        quotechar = '"',
-        normalizenames = false,
-        ntasks = effective_ntasks > 1
+    read_options = Dict(
+        :delim => delim,
+        :decimal => decimal,
+        :header => col_names === true ? 1 : 0,
+        :skipto => skipto + 1,
+        :select => col_select,
+        :groupmark => groupmark,
+        :footerskip => 0,
+        :limit => limit,
+        :comment => comment,
+        :missingstring => missingstring,
+        :escapechar => escape_double ? '"' : '\\',
+        :quotechar => '"',
+        :normalizenames => false,
+        :ntasks => effective_ntasks > 1,
+        :types => col_types
     )
+
+    # Merge additional keyword arguments into the read_options dictionary
+    merge!(read_options, kwargs)
 
     # Initialize an empty DataFrame
     final_df = DataFrame()
@@ -251,7 +269,10 @@ function read_csv2(files;
                   missingstring="",
                   escape_double=true,
                   ntasks::Int = Threads.nthreads(),
-                  num_threads::Union{Int, Nothing}=nothing)
+                  num_threads::Union{Int, Nothing}=nothing,
+                  col_types = nothing,
+                  kwargs...
+                  )
 
     # Normalize input to always be a vector of files
     file_list = (typeof(files) <: AbstractString) ? [files] : files
@@ -266,22 +287,25 @@ function read_csv2(files;
     skipto = skip + (col_names === true ? 1 : 0)
 
     # Prepare CSV reading options
-    read_options = (
-        delim = delim,
-        decimal = decimal,
-        header = col_names === true ? 1 : 0,
-        groupmark = groupmark,
-        skipto = skipto + 1,
-        footerskip = 0,
-        select = col_select,
-        limit = limit,
-        comment = comment,
-        missingstring = missingstring,
-        escapechar = escape_double ? '"' : '\\',
-        quotechar = '"',
-        normalizenames = false,
-        ntasks = effective_ntasks > 1
+    read_options = Dict(
+        :delim => delim,
+        :decimal => decimal,
+        :header => col_names === true ? 1 : 0,
+        :skipto => skipto + 1,
+        :select => col_select,
+        :groupmark => groupmark,
+        :footerskip => 0,
+        :limit => limit,
+        :comment => comment,
+        :missingstring => missingstring,
+        :escapechar => escape_double ? '"' : '\\',
+        :quotechar => '"',
+        :normalizenames => false,
+        :ntasks => effective_ntasks > 1,
+        :types => col_types
     )
+    
+    merge!(read_options, kwargs)
 
     # Initialize an empty DataFrame
     final_df = DataFrame()
