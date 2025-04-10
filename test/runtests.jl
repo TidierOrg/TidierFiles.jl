101f56b74e443e492e0c56b9b6bd21ce21b6956e
stensmo <141365386+stensmo@users.noreply.github.com>
2025-04-10 17:15:45 +0200

    Added back some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ module TestTidierFiles
 using TidierFiles
 using Test
 using Documenter
-#=
+
 DocMeta.setdocmeta!(TidierFiles, :DocTestSetup, :(begin
     using DataFrames, TidierFiles
 end); recursive=true)
 
 doctest(TidierFiles)
-=#
+
 
 @testset "JSON Test" begin
     
